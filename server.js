a43a684f3cf5463941f7c10fa71ba42022549827
NikitaGurba <150122791+NikitaGurba@users.noreply.github.com>
2023-11-28 12:16:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -22,46 +22,109 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
+  socket.on("like", async (id, ip) => {
+    const comment = (await Comment.find({ _id: id }))[0];
+    const likesList = comment.likes;
+    const dislikesList = comment.dislikes;
+    const likesLength = likesList.length;
+    for (var i = 0; i < likesList.length; i++) {
+      if (likesList[i] === ip) {
+        likesList.splice(i, 1);
+        break;
+      }
+    }
+    if (likesLength === likesList.length) {
+      likesList.push(ip);
+    }
+    for (var i = 0; i < dislikesList.length; i++) {
+      if (dislikesList[i] === ip) {
+        dislikesList.splice(i, 1);
+        break;
+      }
+    }
+    let newvalue1 = { $set: { likes: likesList } };
+    let newvalue2 = { $set: { dislikes: dislikesList } };
+    socket.emit("takeRate", likesList, dislikesList);
+    await Comment.updateOne({ _id: id }, newvalue1);
+    await Comment.updateOne({ _id: id }, newvalue2);
+  });
+  socket.on("dislike", async (id, ip) => {
+    const comment = (await Comment.find({ _id: id }))[0];
+    const likesList = comment.likes;
+    const dislikesList = comment.dislikes;
+    const dislikesLength = dislikesList.length;
+    for (var i = 0; i < dislikesList.length; i++) {
+      if (dislikesList[i] === ip) {
+        dislikesList.splice(i, 1);
+        break;
+      }
+    }
+    if (dislikesLength === dislikesList.length) {
+      dislikesList.push(ip);
+    }
+    for (var i = 0; i < likesList.length; i++) {
+      if (likesList[i] === ip) {
+        likesList.splice(i, 1);
+        break;
+      }
+    }
+    let newvalue1 = { $set: { likes: likesList } };
+    let newvalue2 = { $set: { dislikes: dislikesList } };
+    socket.emit("takeRate", likesList, dislikesList);
+    await Comment.updateOne({ _id: id }, newvalue1);
+    await Comment.updateOne({ _id: id }, newvalue2);
+  });
   socket.on("getData", async () => {
     const comments = await Comment.find({}).lean();
     socket.emit("takeData", comments);
   });
   socket.on("takeData", async (text, grade, lecturer) => {
-    let request_json = {
-      message: `условие: является ли этот отзыв приемлимым(без мата, оскорблений и рекламы): '${text}', отвечай в json формате: "YES" или "NO"`,
-      api_key: process.env.CHAD_API_KEY,
-    };
-    request.post(
-      "https://ask.chadgpt.ru/api/public/gpt-3.5",
-      { json: request_json },
-      async function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          console.log(body.response);
-          if (body.response.indexOf("YES") !== -1) {
-            const comment = new Object({
-              grade: grade,
-              text: text.slice(0, MAX_TEXT_LENGTH),
-              lecturer: lecturer.slice(0, MAX_TEXT_LENGTH),
-              date: new Date(),
-              name: "Anonymous",
-              likes: [],
-              dislikes: []
-            });
+    const comment = new Object({
+      grade: grade,
+      text: text.slice(0, MAX_TEXT_LENGTH),
+      lecturer: lecturer.slice(0, MAX_TEXT_LENGTH),
+      date: new Date(),
+      name: "Anonymous",
+      likes: [],
+      dislikes: [],
+    });
 
-            await Comment.collection.insertOne(comment);
-            socket.emit("dataWriteSuccess");
-          } else {
-            socket.emit("dataWriteFail", 'Недопустимый отзыв');
-          }
-        } else {
-          socket.emit("dataWriteFail", error);
-        }
-      }
-    );
+    await Comment.collection.insertOne(comment);
+    socket.emit("dataWriteSuccess");
+    // let request_json = {
+    //   message: `условие: является ли этот отзыв приемлимым, без мата и оскорблений: '${text}', отвечай в json формате: "YES" или "NO"`,
+    //   api_key: process.env.CHAD_API_KEY,
+    // };
+    // request.post(
+    //   "https://ask.chadgpt.ru/api/public/gpt-3.5",
+    //   { json: request_json },
+    //   async function (error, response, body) {
+    //     if (!error && response.statusCode == 200) {
+    //       console.log(body.response);
+    //       if (body.response.indexOf("YES") !== -1) {
+    //         const comment = new Object({
+    //           grade: grade,
+    //           text: text.slice(0, MAX_TEXT_LENGTH),
+    //           lecturer: lecturer.slice(0, MAX_TEXT_LENGTH),
+    //           date: new Date(),
+    //           name: "Anonymous",
+    //           likes: [],
+    //           dislikes: [],
+    //         });
+
+    //         await Comment.collection.insertOne(comment);
+    //         socket.emit("dataWriteSuccess");
+    //       } else {
+    //         socket.emit("dataWriteFail", "Недопустимый отзыв");
+    //       }
+    //     } else {
+    //       socket.emit("dataWriteFail", error);
+    //     }
+    //   }
+    // );
   });
   socket.on("getSpecificData", async (lecturer) => {
     const comments = await Comment.find({}).lean();
-    console.log(comments)
     const newArr = [];
     comments.map((item) => {
       if (item.lecturer == lecturer) {
@@ -70,39 +133,6 @@ io.on("connection", (socket) => {
     });
     socket.emit("takeSpecificData", newArr);
   });
-  socket.on("like", async (id, ip) => {
-    const comment = await Comment.findOne({_id: id});
-    const likesList = comment.likes;
-    const index = likesList.indexOf(ip)
-    if (index !== -1)
-    {
-      likesList.splice(index, 1);
-    }
-    else
-    {
-      likesList.push(ip)
-    }
-    let newvalue = { $set: {likes: likesList} };
-    socket.emit("takeLikes", likesList)
-    await Comment.updateOne({_id: id}, newvalue);
-    
-  })
-  socket.on("dislike", async (id, ip) => {
-    const comment = await Comment.findOne({_id: id});
-    const dislikesList = comment.dislikes;
-    const index = dislikesList.indexOf(ip)
-    if (index !== -1)
-    {
-      dislikesList.splice(index, 1);
-    }
-    else
-    {
-      dislikesList.push(ip)
-    }
-    let newvalue = { $set: {dislikes: dislikesList} };
-    socket.emit("takeDislikes", dislikesList)
-    await Comment.updateOne({_id: id}, newvalue);
-  })
 });
 
 async function start() {
